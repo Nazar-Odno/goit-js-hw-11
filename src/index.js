25b14cfe64e4fb144d92121f5c1451f003956d6f
Nazar-Odno <114081040+Nazar-Odno@users.noreply.github.com>
2023-09-03 21:09:20 +0300

    homework

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { elements } from './js/elem';
 import { BASE_URL, options } from './js/photo api';
 
-searchForm.addEventListener('submit', onFormSybmit);
-window.addEventListener('scroll', onScrollHandler);
-document.addEventListener('DOMContentLoaded', hideLoader);
-
-function showLoader() {
-  loaderEl.style.display = 'block';
-}
-
-function hideLoader() {
-  loaderEl.style.display = 'none';
-}
-
 const { galleryEl, searchInput, searchForm, loaderEl } = elements;
 
 let totalHits = 0;
@@ -31,6 +19,18 @@ const lightbox = new SimpleLightbox('.lightbox', {
   scrollZoom: false,
   close: false,
 });
+
+searchForm.addEventListener('submit', onFormSybmit);
+window.addEventListener('scroll', onScrollHandler);
+document.addEventListener('DOMContentLoaded', hideLoader);
+
+function showLoader() {
+  loaderEl.style.display = 'block';
+}
+
+function hideLoader() {
+  loaderEl.style.display = 'none';
+}
 
 function renderGallery(hits) {
   const markup = hits
@@ -91,46 +91,46 @@ async function loadMore() {
   }
 }
 
-async function onFormSybmit(e) {
-    e.preventDefault();
-    options.params.q = searchInput.value.trim();
-    if (options.params.q === '') {
-      return;
-    }
-    options.params.page = 1;
-    galleryEl.innerHTML = '';
-    reachedEnd = false;
-  
-    try {
-      showLoader();
-      const response = await axios.get(BASE_URL, options);
-      totalHits = response.data.totalHits;
-      const hits = response.data.hits;
-      if (hits.length === 0) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        Notify.success(`Hooray! We found ${totalHits} images.`);
-        renderGallery(hits);
-      }
-      searchInput.value = '';
-      hideLoader();
-    } catch (err) {
-      Notify.failure(err);
-      hideLoader();
-    }
+function onScrollHandler() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  const scrollThreshold = 300;
+  if (
+    scrollTop + clientHeight >= scrollHeight - scrollThreshold &&
+    galleryEl.innerHTML !== '' &&
+    !isLoadingMore &&
+    !reachedEnd
+  ) {
+    loadMore();
   }
+}
 
-  function onScrollHandler() {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-    const scrollThreshold = 300;
-    if (
-      scrollTop + clientHeight >= scrollHeight - scrollThreshold &&
-      galleryEl.innerHTML !== '' &&
-      !isLoadingMore &&
-      !reachedEnd
-    ) {
-      loadMore();
-    }
+async function onFormSybmit(e) {
+  e.preventDefault();
+  options.params.q = searchInput.value.trim();
+  if (options.params.q === '') {
+    return;
   }
+  options.params.page = 1;
+  galleryEl.innerHTML = '';
+  reachedEnd = false;
+
+  try {
+    showLoader();
+    const response = await axios.get(BASE_URL, options);
+    totalHits = response.data.totalHits;
+    const hits = response.data.hits;
+    if (hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+      renderGallery(hits);
+    }
+    searchInput.value = '';
+    hideLoader();
+  } catch (err) {
+    Notify.failure(err);
+    hideLoader();
+  }
+}
